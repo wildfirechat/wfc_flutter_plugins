f71d7b586182e80c0103fcbad1bff3f20559cf74
imndx <imndxx@gmail.com>
2023-03-09 23:22:09 +0800

    fix #40

--- example/android/app/src/main/java/cn/wildfirechat/wfc_example/MainActivity.java
+++ example/android/app/src/main/java/cn/wildfirechat/wfc_example/MainActivity.java
@@ -1,6 +1,14 @@
 package cn.wildfirechat.wfc_example;
 
+import androidx.annotation.NonNull;
+import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
 
 public class MainActivity extends FlutterActivity {
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
 }
