f1f5d04e0e58154ecad662521bb1d4777649f7a6
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2025-07-17 14:16:31 +0800

    升级音视频SDK和添加推送注册

--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -1,13 +1,63 @@
 #import "AppDelegate.h"
 #import "GeneratedPluginRegistrant.h"
+#import <UserNotifications/UserNotifications.h>
+#include <WFChatClient/WFCChatClient.h>
+
+@interface AppDelegate () <UNUserNotificationCenterDelegate>
+
+@end
 
 @implementation AppDelegate
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [GeneratedPluginRegistrant registerWithRegistry:self];
 
-- (BOOL)application:(UIApplication *)application
-    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    center.delegate = self;
+
+    [center requestAuthorizationWithOptions:(UNAuthorizationOptionAlert | UNAuthorizationOptionSound | UNAuthorizationOptionBadge)
+                          completionHandler:^(BOOL granted, NSError * _Nullable error) {
+                              if (granted) {
+                                  dispatch_async(dispatch_get_main_queue(), ^{
+                                      [[UIApplication sharedApplication] registerForRemoteNotifications];
+                                  });
+                                  
+                              } else {
+                              }
+                          }];
+
+
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:
+(UIUserNotificationSettings *)notificationSettings {
+    // register to receive notifications
+    [application registerForRemoteNotifications];
+}
+
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    if ([deviceToken isKindOfClass:[NSData class]]) {
+        const unsigned *tokenBytes = [deviceToken bytes];
+        NSString *hexToken = [NSString stringWithFormat:@"%08x%08x%08x%08x%08x%08x%08x%08x",
+                              ntohl(tokenBytes[0]), ntohl(tokenBytes[1]), ntohl(tokenBytes[2]),
+                              ntohl(tokenBytes[3]), ntohl(tokenBytes[4]), ntohl(tokenBytes[5]),
+                              ntohl(tokenBytes[6]), ntohl(tokenBytes[7])];
+        [[WFCCNetworkService sharedInstance] setDeviceToken:hexToken];
+    } else {
+        NSString *token = [[[[deviceToken description] stringByReplacingOccurrencesOfString:@"<"
+                                                                                 withString:@""]
+                            stringByReplacingOccurrencesOfString:@">"
+                            withString:@""]
+                           stringByReplacingOccurrencesOfString:@" "
+                           withString:@""];
+        
+        [[WFCCNetworkService sharedInstance] setDeviceToken:token];
+    }
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    
 }
 
 @end
